a89b402dd4f57c5104571edd9e7bdc396ecf43db
Reg22222 <103514126+reg22222@users.noreply.github.com>
2023-03-22 11:02:27 +0900

    Add files via upload

---
+++ Javaclass.java
@@ -0,0 +1,10 @@
+public class Javaclass {
+	public static void main(String[] args) {
+		int[] a = new int[5];
+		for(int i = 0; i <a.length ; i++)
+		{
+			System.out.println("a["+i+"]="+a[i]);
+			
+		}
+	}
+}
